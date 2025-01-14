1a6e8d7f2342fad719085f15f1445ad1087a6c36
Nikita Platonov <nplatonov458@gmail.com>
2025-01-14 14:43:37 +0400

    Создан интерфейс создания окна -> WindowGame

---
+++ src/main/java/ru/project/GameInterface/WindowGame.java
@@ -0,0 +1,8 @@
+package ru.project.GameInterface;
+
+import java.awt.*;
+import java.io.IOException;
+
+public interface WindowGame {
+    void createWindow(String imgBackgroungPath, String imgSnakePath, String imgApplePath) throws IOException;
+}
