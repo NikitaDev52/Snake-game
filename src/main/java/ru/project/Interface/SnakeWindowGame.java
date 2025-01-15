ee56377b3e528c79dc80f89ff418f1f22d825f27
Nikita Platonov <nplatonov458@gmail.com>
2025-01-15 11:25:32 +0400

    Изменен таймер: теперь после каждого съедения яблока проверяется условие на победу

--- src/main/java/ru/project/Interface/SnakeWindowGame.java
+++ src/main/java/ru/project/Interface/SnakeWindowGame.java
@@ -4,18 +4,18 @@ import ru.project.Logics.Apple;
 import ru.project.Logics.SnakePanel;
 import ru.project.Resources;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
 public class SnakeWindowGame implements WindowGame {
-    private final BufferedImage imgBackgroung = Resources.imgBackgroung;
+    private final BufferedImage imgBackgroung = Resources.IMG_BACKGROUNG;
     private final SnakePanel snake = new SnakePanel();
-    private final Apple apple = new Apple(Resources.imgBackgroung.getWidth(), Resources.imgBackgroung.getHeight());
+    private final Apple apple = new Apple(Resources.IMG_BACKGROUNG.getWidth(), Resources.IMG_BACKGROUNG.getHeight());
+    // 3 - изначальный размер змейки
+    private int score = snake.getSnakeSize() - 3;
 
     public SnakeWindowGame() throws IOException {
     }
@@ -23,25 +23,32 @@ public class SnakeWindowGame implements WindowGame {
     @Override
     public void createWindow() {
             JFrame frame = new JFrame("Snake game");
-            frame.setSize(Resources.imgBackgroung.getWidth(), Resources.imgBackgroung.getHeight());
+            frame.setSize(Resources.IMG_BACKGROUNG.getWidth(), Resources.IMG_BACKGROUNG.getHeight());
             // фон для змейки
             JPanel panel = new JPanel() {
+
                 @Override
                 protected void paintComponent(Graphics g) {
                     super.paintComponent(g);
-                    Image backgroundImage = Toolkit.getDefaultToolkit().getImage(Resources.imgBackgroungPath);
+                    Image backgroundImage = Toolkit.getDefaultToolkit().getImage(Resources.IMG_BACKGROUNG_PATH);
                     g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
 
                     snake.paintSnake(g);
 
                     apple.paintApple(g);
+
+                    g.setColor(Color.WHITE);
+                    g.setFont(new Font(Resources.FONT_TEXT, Font.BOLD, Resources.TEXT_SIZE));
+                    g.drawString(Resources.VICTORY_CONDITIONS + "Твой счет: " + score, 10, 20);
                 }
             };
-            frame.add(panel);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setVisible(true);
+        panel.setPreferredSize(new Dimension(Resources.IMG_BACKGROUNG.getWidth(), Resources.IMG_BACKGROUNG.getHeight()));
+        frame.add(panel);
+        frame.pack();
+        frame.setVisible(true);
 
-            frame.addKeyListener(new java.awt.event.KeyAdapter() {
+
+        frame.addKeyListener(new java.awt.event.KeyAdapter() {
                 @Override
                 public void keyPressed(java.awt.event.KeyEvent e) {
                     switch (e.getKeyCode()) {
@@ -60,8 +67,12 @@ public class SnakeWindowGame implements WindowGame {
             // Проверяем, съела ли змейка яблоко
             Point appleGridPosition = new Point(apple.getPosition().x / snake.getTileSize(), apple.getPosition().y / snake.getTileSize());
             if (snake.getHead().equals(appleGridPosition)) {
+                if (score == Resources.SCORE_WIN) {
+                    snake.endGame(Resources.MESSAGE_WIN);
+                }
+                score += 1;
                 snake.grow();
-                apple.generateNewApplePos(Resources.imgBackgroung.getWidth(), Resources.imgBackgroung.getHeight());
+                apple.generateNewApplePos(Resources.IMG_BACKGROUNG.getWidth(), Resources.IMG_BACKGROUNG.getHeight());
             }
             panel.repaint();
         });
