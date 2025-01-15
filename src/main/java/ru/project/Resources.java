3f0a4de9025a629f737ec1ea0ec4cb93858ef5fb
Nikita Platonov <nplatonov458@gmail.com>
2025-01-15 11:22:44 +0400

    Теперь есть возможность менять условия игры не меняя исходный код

--- src/main/java/ru/project/Resources.java
+++ src/main/java/ru/project/Resources.java
@@ -1,18 +1,26 @@
 package ru.project;
 
 import javax.imageio.ImageIO;
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class Resources {
-    public static final BufferedImage imgBackgroung;
-    public static final String imgBackgroungPath = "src/main/resources/IMGs/фон для змейки.png";
+    public static final BufferedImage IMG_BACKGROUNG;
+    public static final String IMG_BACKGROUNG_PATH = "src/main/resources/IMGs/фон для змейки.png";
+
+    public static final int SIZE_OBJ = 20;
+    public static final int SCORE_WIN = 50;
+    public static final int TEXT_SIZE = 18;
+
+    public static final String MESSAGE_WIN = "Ты победил!";
+    public static final String FONT_TEXT = "Arial";
+    public static final String VICTORY_CONDITIONS = "Собери " + SCORE_WIN + " яблок чтобы победить! ";
+
 
     static {
         try {
-            imgBackgroung = ImageIO.read(new File(imgBackgroungPath));
+            IMG_BACKGROUNG = ImageIO.read(new File(IMG_BACKGROUNG_PATH));
         } catch (IOException e) {
             throw new RuntimeException("Не удалось найти или открыть фото :( , стек: " + e);
         }
